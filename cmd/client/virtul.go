979bd4239d0ea1c682a6c67c658c7c04fca1ba95
WangZY <76583642+xmx@users.noreply.github.com>
2026-05-26 19:15:58 +0800

    新增 websocket 示例

--- cmd/client/virtul.go
+++ cmd/client/virtul.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"crypto/rand"
+	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/websocket"
 )
 
 func NewVirtual() http.Handler {
@@ -20,12 +24,38 @@ func NewVirtual() http.Handler {
 
 		io.Copy(w, big)
 	})
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("hello233\n"))
-	})
+	mux.HandleFunc("/api/chat", chat)
 
 	// curl -L "http://127.0.0.1:9999/api/direct/api/download?id=1" -o /dev/null
 
 	return mux
+}
+
+func chat(w http.ResponseWriter, r *http.Request) {
+	upg := &websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
+		return true
+	}}
+	ws, err := upg.Upgrade(w, r, nil)
+	if err != nil {
+		slog.Error("websocket upgrade failed", "err", err)
+		return
+	}
+	defer ws.Close()
+
+	ws.WriteMessage(websocket.TextMessage, []byte("你好，websocket 连接成功了"))
+
+	for {
+		typ, msg, err := ws.ReadMessage()
+		if err != nil {
+			slog.Error("消息读取出错", "err", err)
+			return
+		}
+
+		fmt.Printf(">>> %s\n", msg)
+
+		if err = ws.WriteMessage(typ, append([]byte("虚拟通道已收到："), msg...)); err != nil {
+			slog.Error("消息回写出错", "err", err)
+		}
+	}
+
 }
